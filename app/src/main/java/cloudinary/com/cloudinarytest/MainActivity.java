32d1b6c1e84fad8435f73b25cf7c2525d7525591
Leigh R. Dinaya <leighdinaya04@gmail.com>
2016-04-12 16:39:03 +0800

    Added method to select photo on gallery and an asynctask to save the data to cloudinary

--- app/src/main/java/cloudinary/com/cloudinarytest/MainActivity.java
+++ app/src/main/java/cloudinary/com/cloudinarytest/MainActivity.java
@@ -2,6 +2,7 @@ package cloudinary.com.cloudinarytest;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +11,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mImageView;
     protected Uri mMediaUri;
     private Cloudinary cloudinary;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +43,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             
+                EasyImage.openGallery(MainActivity.this, 0);
             }
         });
 
         Map config = new HashMap();
         config.put("cloud_name", "rentapp");
-        config.put("api_key", "123456789012345");
+        config.put("api_key", "834633977881291");
         config.put("api_secret", "Q11RAIP6TdssH2T89vFWZTsFN0Q");
         cloudinary = new Cloudinary(config);
 
@@ -50,7 +58,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new EasyImage.Callbacks() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
 
+            }
+
+            @Override
+            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                file = imageFile;
+                new UploadImage().execute();
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+
+            }
+        });
     }
 
 
@@ -74,5 +98,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EasyImage.clearConfiguration(this);
+        super.onDestroy();
+
+    }
+
+    private class UploadImage extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getApplicationContext(), "Image uploaded.", Toast.LENGTH_SHORT).show();
+            super.onPostExecute(s);
+        }
     }
 }
